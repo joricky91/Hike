604dfdca1a2688a5dfb53904202d3bf69fb4de1f
joricky91 <jonathanricky91@gmail.com>
2023-11-23 18:55:11 +0700

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Jonathan Ricky Sandjaja on 23/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(.image1)
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Jonathan Ricky Sandjaja on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
