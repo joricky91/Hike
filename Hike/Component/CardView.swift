84cf9c322522f57bffd3bc72bdca2839c5432580
joricky91 <jonathanricky91@gmail.com>
2023-11-24 13:56:23 +0700

    feat: add card header

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -10,16 +10,52 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
-                )
-                .frame(width: 256, height: 256)
+            CustomBackgroundView()
             
-            Image(.image1)
-                .resizable()
-            .scaledToFit()
+            VStack {
+                //MARK: - HEADER
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52)) 
+                        .foregroundStyle(.linearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // Show a sheet
+                            print("The button was pressed")
+                        }, label: {
+                            CustomButtonView()
+                        })
+                    }
+                    
+                    Text("Find and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+                
+                //MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    
+                    Image(.image1)
+                        .resizable()
+                    .scaledToFit()
+                }
+                
+                //MARK: - FOOTER
+            }
         }
+        .frame(width: 320, height: 570)
     }
 }
 
---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by Jonathan Ricky Sandjaja on 24/11/23.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(LinearGradient(colors: [.white, .customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom))
+                
+            Circle()
+                .stroke(.linearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom), lineWidth: 4)
+            
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(.linearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+        }
+        .frame(width: 58, height: 58)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
