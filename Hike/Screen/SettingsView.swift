d4f55fa0300581f1a15da57668190a07402be96b
joricky91 <jonathanricky91@gmail.com>
2023-11-25 13:38:50 +0700

    feat: creating list row content

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -54,6 +54,33 @@ struct SettingsView: View {
             }
             .listRowSeparator(.hidden)
             // MARK: - SECTION ICONS
+            Section {
+                // Basic labeled content
+//                LabeledContent("Application", value: "Hike")
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "Hike", rowTintColor: .blue)
+                
+                CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS & iPadOS", rowTintColor: .red)
+                
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "Jonathan Ricky", rowTintColor: .mint)
+                
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
+                
+                CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Academy", rowLinkDestination: "https://credo.academy")
+            } header: {
+                Text("ABOUT THE APP")
+            } footer: {
+                HStack {
+                    Spacer()
+                    
+                    Text("Copyright © All rights reserved.")
+                    
+                    Spacer()
+                }.padding(.vertical, 8)
+            }
             
             // MARK: - SECTION ABOUT
         }
